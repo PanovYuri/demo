e9aef88480bd6e1f8999f1ff6efd044bd0ccc0cf
Panov Yuri <35775430+PanovYuri@users.noreply.github.com>
2020-03-12 01:55:55 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://sysout.ru/chat-na-spring-boot-i-websocket/
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
